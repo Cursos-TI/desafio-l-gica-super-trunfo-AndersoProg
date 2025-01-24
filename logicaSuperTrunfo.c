40450e0fe4260064361ecb4e70e79da1ccf2b31f
AndersoProg <andersonvideira2015@icloud.com>
2025-01-24 19:40:02 +0000

    Cadastrei as duas cartas do jogo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -39,5 +39,70 @@ int main() {
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
+float populacao;
+float area;
+float PIB;
+int pontos;
+char Cidade [20];
+
+
+printf("\n\nS u p er  -  t r u n fo - LÓGICA! \n"); // tema do jogo
+printf("--------------------------------\n");
+printf("\nA01 - BRASIL \n\n"); // título
+
+          printf("ESTADO -CEARA\n\n"); // subtítulo Enfatizando o estado
+
+        printf("Digite a cidade: \n"); // digite a cidade
+        scanf("%s", &Cidade); // armazenando os dados digitados pelo o úsuario
+            printf("Digite a população:  \n");
+            scanf("%f", &populacao); //armazenando os dados digitados pelo o úsuario
+              printf("Digite a area em (km2): \n");
+              scanf("%f", &area); //armazenando os dados digitados pelo o úsuario
+                printf("Digite o PIB: \n");
+                scanf("%f", &PIB); //armazenando os dados digitados pelo o úsuario
+                  printf("Digite a quantidade de pontos turisticos: \n");
+                  scanf("%d", &pontos); //armazenando os dados digitados pelo o úsuario
+printf("----------------------------------------------\n"); 
+printf("\nPaís BRASIL!!"); //apresentação na tela para o usuário.
+printf("\n\nCOD-A01 - ( ESTADO DO CEARA )\n"); //apresentação na tela para o usuário.
+printf("Cidade: %s\n", Cidade); //apresentação na tela para o usuário.
+printf("\nPopulação é: %.3f de habitantes\n", populacao); //apresentação na tela para o usuário.
+printf("Area total em (km2) é de: %.3f\n", area); //apresentação na tela para o usuário.
+printf("O PIB é de: R$:%.2f de reais\n", PIB); //apresentação na tela para o usuário.
+printf("Pontos turisticos: %d\n\n\n", pontos); ////apresentação na tela para o usuário
+
+                                       // fim do bloco de cadastro primeira CARTA 01
+
+printf("--------------------------------\n");
+printf("\nA02 - BRASIL \n\n"); // título
+
+          printf("ESTADO - SÃO PAULO\n\n"); // subtítulo enfatizando o estado (para experiência do usário na tela)
+
+        printf("Digite a cidade: \n"); // digite a cidade
+        scanf("%s", &Cidade); // armazenando os dados digitados pelo o úsuario
+            printf("Digite a população:  \n");
+            scanf("%f", &populacao); // armazenando os dados digitados pelo o úsuario
+              printf("Digite a area em (km2): \n");
+              scanf("%f", &area); // armazenando os dados digitados pelo o úsuario
+                printf("Digite o PIB: \n");
+                scanf("%f", &PIB); // armazenando os dados digitados pelo o úsuario
+                  printf("Digite a quantidade de pontos turisticos: \n");
+                  scanf("%d", &pontos); // armazenando os dados digitados pelo o úsuario
+printf("----------------------------------------------\n");
+printf("\nPaís BRASIL!!"); //apresentação na tela para o usuário.
+printf("\n\nA02 - ( SÃO PAULO )\n"); //apresentação na tela para o usuário.
+printf("Cidade:%s \n", Cidade); //apresentação na tela para o usuário.
+printf("População é: %.3f de habitantes\n", populacao); //apresentação na tela para o usuário.
+printf("Area total em (km2) é de: %.3f\n", area); ///apresentação na tela para o usuário.
+printf("O PIB é de: R$:%.2f de reais\n", PIB); //apresentação na tela para o usuário.
+printf("Pontos turisticos: %d\n\n\n", pontos); //apresentação na tela para o usuário.
+
+                                              // fim do bloco de cadastro primeira CARTA 02
+
+printf("\n\n----------------------------------------------\n\n");
+printf("A CARTA VECEDORA É: \n");
+
+
+
     return 0;
 }
